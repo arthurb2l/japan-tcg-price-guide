3a01a11b84f3a0a7ad40dde4eb092f39598edc1c
Arthur Brejon <arthurb2l@users.noreply.github.com>
2026-03-17 21:30:01 +0900

    Switch to network-first caching for fresh prices

--- sw.js
+++ sw.js
@@ -1,22 +1,17 @@
-const CACHE = 'tcg-v1';
-const ASSETS = [
-  '/japan-tcg-price-guide/',
-  '/japan-tcg-price-guide/pokemon/',
-  '/japan-tcg-price-guide/onepiece/',
-  '/japan-tcg-price-guide/favicon.ico'
-];
+const CACHE = 'tcg-v2';
 
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', e => e.waitUntil(
+  caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+));
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
-      if (res.ok && e.request.url.includes('japan-tcg-price-guide')) {
-        caches.open(CACHE).then(c => c.put(e.request, res.clone()));
-      }
-      return res;
-    }))
+    fetch(e.request)
+      .then(res => {
+        if (res.ok) caches.open(CACHE).then(c => c.put(e.request, res.clone()));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
   );
 });
