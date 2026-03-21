ba4767fd0930a166753ab8f12161e24b7aef6fc5
Arthur Brejon <arthurb2l@users.noreply.github.com>
2026-03-21 16:26:33 +0900

    feat: Expand database - 9,208 Pokemon + 32 One Piece (#59, #61)
    Pokemon (9,208 cards, 53 sets):
    - Added Base Set, Jungle, Fossil (vintage)
    - Added Neo Discovery, Neo Revelation
    - Added XY base, Flashfire, Furious Fists, Roaring Skies, Ancient Origins
    - Added SWSH base, Evolving Skies, Shining Fates, Vivid Voltage, Champion's Path
    
    One Piece (32 cards, 14 sets):
    - Added 15 more high-value cards
    - Covers OP-01 through OP-09, PRB-01/02, ST-01/10/13
    
    File size: 4.7MB (approaching 5MB sharding threshold)

--- scripts/onepiece-manual.js
+++ scripts/onepiece-manual.js
@@ -33,26 +33,49 @@ const FINDABLE_CARDS = [
   
   // OP-03 Pillars of Strength
   { id: "OP03-112", name: "Boa Hancock", nameJp: "ボア・ハンコック", set: "OP-03", setName: "Pillars of Strength", rarity: "SR", variant: "Alt Art", price: { jpy: 18000, eur: 111 }, img: null },
+  { id: "OP03-099", name: "Charlotte Katakuri", nameJp: "シャーロット・カタクリ", set: "OP-03", setName: "Pillars of Strength", rarity: "SEC", variant: "Manga", price: { jpy: 15000, eur: 93 }, img: null },
   
   // OP-04 Kingdoms of Intrigue
   { id: "OP04-024", name: "Yamato", nameJp: "ヤマト", set: "OP-04", setName: "Kingdoms of Intrigue", rarity: "SR", variant: "Alt Art", price: { jpy: 12000, eur: 74 }, img: null },
+  { id: "OP04-112", name: "Sabo", nameJp: "サボ", set: "OP-04", setName: "Kingdoms of Intrigue", rarity: "SEC", variant: "Manga", price: { jpy: 20000, eur: 123 }, img: null },
   
   // OP-06 Wings of the Captain
   { id: "OP06-086", name: "Roronoa Zoro", nameJp: "ロロノア・ゾロ", set: "OP-06", setName: "Wings of the Captain", rarity: "SEC", variant: "Manga", price: { jpy: 45000, eur: 278 }, img: null },
+  { id: "OP06-118", name: "Sanji", nameJp: "サンジ", set: "OP-06", setName: "Wings of the Captain", rarity: "SEC", variant: "Manga", price: { jpy: 18000, eur: 111 }, img: null },
   
   // OP-07 500 Years in the Future
   { id: "OP07-109", name: "Eustass Kid", nameJp: "ユースタス・キッド", set: "OP-07", setName: "500 Years in the Future", rarity: "SEC", variant: "Manga", price: { jpy: 25000, eur: 154 }, img: null },
   { id: "OP07-118", name: "Jewelry Bonney", nameJp: "ジュエリー・ボニー", set: "OP-07", setName: "500 Years in the Future", rarity: "SEC", variant: "Alt Art", price: { jpy: 15000, eur: 93 }, img: null },
+  { id: "OP07-015", name: "Vegapunk", nameJp: "ベガパンク", set: "OP-07", setName: "500 Years in the Future", rarity: "SR", variant: "Alt Art", price: { jpy: 8000, eur: 49 }, img: null },
   
   // OP-08 Two Legends
   { id: "OP08-106", name: "Silvers Rayleigh", nameJp: "シルバーズ・レイリー", set: "OP-08", setName: "Two Legends", rarity: "SEC", variant: "Manga", price: { jpy: 30000, eur: 185 }, img: null },
   { id: "OP08-118", name: "Monkey D. Garp", nameJp: "モンキー・D・ガープ", set: "OP-08", setName: "Two Legends", rarity: "SEC", variant: "Manga", price: { jpy: 22000, eur: 136 }, img: null },
+  { id: "OP08-058", name: "Whitebeard", nameJp: "白ひげ", set: "OP-08", setName: "Two Legends", rarity: "L", variant: "Alt Art", price: { jpy: 12000, eur: 74 }, img: null },
+  
+  // OP-09 Four Emperors (non-grail)
+  { id: "OP09-072", name: "Kaido", nameJp: "カイドウ", set: "OP-09", setName: "Four Emperors", rarity: "SEC", variant: "Alt Art", price: { jpy: 35000, eur: 216 }, img: null },
+  { id: "OP09-119", name: "Big Mom", nameJp: "ビッグ・マム", set: "OP-09", setName: "Four Emperors", rarity: "SEC", variant: "Manga", price: { jpy: 28000, eur: 173 }, img: null },
   
   // PRB-02 Premium Booster 2
   { id: "PRB02-001", name: "Uta", nameJp: "ウタ", set: "PRB-02", setName: "Premium Booster 2", rarity: "SEC", variant: "Alt Art", price: { jpy: 20000, eur: 123 }, img: null },
+  { id: "PRB02-003", name: "Shanks", nameJp: "シャンクス", set: "PRB-02", setName: "Premium Booster 2", rarity: "SEC", variant: "Film Red", price: { jpy: 15000, eur: 93 }, img: null },
   
   // ST decks chase cards
   { id: "ST10-001", name: "Trafalgar Law", nameJp: "トラファルガー・ロー", set: "ST-10", setName: "Ultimate Deck", rarity: "L", variant: "Alt Art", price: { jpy: 8000, eur: 49 }, img: null },
+  { id: "ST13-003", name: "Monkey D. Luffy", nameJp: "モンキー・D・ルフィ", set: "ST-13", setName: "The Three Brothers", rarity: "L", variant: "Parallel", price: { jpy: 6000, eur: 37 }, img: null },
+  
+  // OP-01 Romance Dawn (more findables)
+  { id: "OP01-025", name: "Nami", nameJp: "ナミ", set: "OP-01", setName: "Romance Dawn", rarity: "SR", variant: "Alt Art", price: { jpy: 8000, eur: 49 }, img: null },
+  { id: "OP01-047", name: "Roronoa Zoro", nameJp: "ロロノア・ゾロ", set: "OP-01", setName: "Romance Dawn", rarity: "SR", variant: "Alt Art", price: { jpy: 10000, eur: 62 }, img: null },
+  
+  // OP-02 Paramount War (more)
+  { id: "OP02-120", name: "Edward Newgate", nameJp: "エドワード・ニューゲート", set: "OP-02", setName: "Paramount War", rarity: "SEC", variant: "Manga", price: { jpy: 40000, eur: 247 }, img: null },
+  { id: "OP02-085", name: "Crocodile", nameJp: "クロコダイル", set: "OP-02", setName: "Paramount War", rarity: "SR", variant: "Alt Art", price: { jpy: 7000, eur: 43 }, img: null },
+  
+  // OP-05 Awakening (more)
+  { id: "OP05-074", name: "Trafalgar Law", nameJp: "トラファルガー・ロー", set: "OP-05", setName: "Awakening of the New Era", rarity: "SR", variant: "Alt Art", price: { jpy: 12000, eur: 74 }, img: null },
+  { id: "OP05-118", name: "Sabo", nameJp: "サボ", set: "OP-05", setName: "Awakening of the New Era", rarity: "SEC", variant: "Alt Art", price: { jpy: 18000, eur: 111 }, img: null },
 ];
 
 function buildCache() {
